300565d4d93fb989ab9cbeb4172ede932cf200b3
sathvika-seshasayee <80007708+sathvika-seshasayee@users.noreply.github.com>
2021-05-24 15:05:52 +0530

    final commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,188 +1,264 @@
-(function(){
-    var features = [
+(function () {
+    var categories = [
         {
             id: 1,
             name: "My Day",
             icon: "far fa-sun",
-            tasks:[]
+            tasks: []
         },
         {
             id: 2,
             name: "Important",
             icon: "far fa-star",
-            tasks:[]
+            tasks: []
         },
         {
             id: 3,
             name: "Planned",
             icon: "far fa-calendar",
-            tasks:[]
+            tasks: []
         },
         {
             id: 4,
             name: "Assigned to you",
             icon: "fas fa-user-alt",
-            tasks:[]
+            tasks: []
         },
         {
             id: 5,
             name: "Tasks",
             icon: "fas fa-home",
-            tasks:[]
+            tasks: []
         }
     ];
+    let taskId;
+    let categoryId = 5;
 
-function init () {
-    var unOrderedList = document.getElementById("left-navigation");
-    for(var feature in features) {
-        var list = document.createElement("li");
-        var span = document.createElement("span");
-        var icon = document.createElement("i");
-        icon.className = features[feature].icon;
-        span.id = features[feature].id;
-        span.appendChild(document.createTextNode(features[feature].name));
-        list.appendChild(icon);
-        list.appendChild(span);
-        unOrderedList.appendChild(list);
+    /**
+     * Function to render left navigation bar for tasks category
+     */
+    function init() {
+        const unOrderedList = document.getElementById("left-navigation");
+        for (const feature in categories) {
+            const list = document.createElement("li");
+            const span = document.createElement("span");
+            const icon = document.createElement("i");
+            icon.className = categories[feature].icon;
+            span.id = categories[feature].id;
+            span.appendChild(document.createTextNode(categories[feature].name));
+            list.appendChild(icon);
+            list.appendChild(span);
+            unOrderedList.appendChild(list);
+        }
+
+        document.getElementById("input-new-task-category").
+            addEventListener("keypress", addNewTaskCategory);
+
+        document.getElementById("new-task-input").
+            addEventListener("keypress", addNewTask);
+
+        document.getElementById("add-step-input").
+            addEventListener("keypress", addNewSubTask);
+
+        document.getElementById("existing-task-list").addEventListener("click", renderSubTask);
+
+        document.getElementById("left-navigation").addEventListener("click", renderTask);
     }
-    document.getElementById("right").style.visibility = "hidden";
-}
-document.addEventListener("mouseover", function(event) {
-    if("LI" === event.target.tagName  || "new-task-category-input" == event.target.name &&
-    event.target.firstChild != document.getElementsByClassName("navigation-border")[0]) {                   // See that span can get clicked also
-        event.target.style.backgroundColor = "#e6e4e4";
-    }  
-});
-document.addEventListener("mouseover", function(event) {
-    if("SPAN" === event.target.tagName  && "left-navigation" === event.target.parentNode.parentNode.id) {                   
-        event.target.parentNode.style.backgroundColor = "#e6e4e4";
-    }  
-});
-document.addEventListener("mouseout", function(event) {
-    if("SPAN" === event.target.tagName  && "left-navigation" === event.target.parentNode.parentNode.id) {                   
-        event.target.parentNode.style.backgroundColor = "#f4f4f4";
-    }  
-});
-document.addEventListener("mouseout", function(event) {
-    if("LI" === event.target.tagName || "new-task-category-input" == event.target.name &&  
-    event.target.firstChild != document.getElementsByClassName("navigation-border")[0]) {
-        event.target.style.backgroundColor = "#f4f4f4";
-    } 
-});
-document.getElementById("input-new-task-category").addEventListener("keypress", function(event){
-    if("Enter" == event.key && document.getElementById("input-new-task-category").value != "") {
-        var newTaskCategory = document.getElementById("input-new-task-category").value;
-        document.getElementById("input-new-task-category").value = "";
-        addNewTaskCategory(newTaskCategory);
+
+    /**
+     * Function to add new tasks category to left navigation bar
+     */
+    function addNewTaskCategory(event) {
+        if ("Enter" == event.key && document.getElementById("input-new-task-category").value != "") {
+            const newTaskCategory = document.getElementById("input-new-task-category").value;
+            document.getElementById("input-new-task-category").value = "";
+            const newTask = {
+                id: categories.length + 1,
+                icon: "fas fa-list-ul",
+                name: newTaskCategory,
+                tasks: []
+            }
+            categories.push(newTask);
+            const unOrderedList = document.getElementById("left-navigation");
+            const list = document.createElement("li");
+            const icon = document.createElement("i");
+            const span = document.createElement("span");
+            icon.className = newTask.icon;
+            span.id = newTask.id;
+            list.append(icon, span, newTask.name);
+            unOrderedList.appendChild(list);
+        }
     }
-})
-document.getElementById("new-sub-task-input").addEventListener("keypress", function(event){
-    if("Enter" == event.key && document.getElementById("new-sub-task-input").value != "") {
-        var newSubTask = document.getElementById("new-sub-task-input").value;
-        document.getElementById("new-sub-task-input").value = "";
-        var taskCategoryId = document.getElementById("center-title").firstChild.nextSibling.id;
-        addNewSubTask(newSubTask, taskCategoryId);
+
+    /**
+     * Function to add new tasks center part
+     */
+    function addNewTask(event) {
+        if ("Enter" == event.key && document.getElementById("new-task-input").value != "") {
+            const newTask = document.getElementById("new-task-input").value;
+            document.getElementById("new-task-input").value = "";
+            const task = {
+                id: (categories[categoryId - 1].tasks.length + 1),
+                name: newTask,
+                checked: false,
+                important:false,
+                subTasks: []
+            }
+            taskId = task.id;
+            categories[categoryId - 1].tasks.push(task);
+            const unOrderedList = document.getElementById("existing-task-list");
+            const list = getTask(task);
+            unOrderedList.appendChild(list);
+
+        }
     }
-})
-function addNewSubTask(newSubTask, taskCategoryId) {
-    var subTask = {
-        id : (features[taskCategoryId - 1].tasks.length + 1) + features[taskCategoryId - 1].name,
-        name : newSubTask,
-        checked:false,
-        subCategory : [] 
+
+     /**
+     * Function to add new sub tasks to right side
+     */
+      function addNewSubTask(event) {
+        if ("Enter" == event.key && document.getElementsByClassName("add-step-input")[0].value != "") {
+            const newSubTask = document.getElementById("add-step-input").value;
+            document.getElementById("add-step-input").value = "";
+            const subTask = {
+                id: categories[categoryId - 1].tasks[taskId - 1].subTasks + 1,
+                name: newSubTask
+            }
+            categories[categoryId - 1].tasks[taskId - 1].subTasks.push(subTask);
+            const unOrderedList = document.getElementsByClassName("sub-task")[0];
+            const list = getSubTask(subTask);
+            unOrderedList.appendChild(list);
+        }
     }
-    features[taskCategoryId - 1].tasks.push(subTask);
-    var unOrderedList = document.getElementById("existing-task-list"); 
-    var list = document.createElement("li");
-    var input = document.createElement("input");
-    var icon = document.createElement("i");
-    var span = document.createElement("span");
-    input.id = subTask.id;
-    icon.className = "far fa-star";
-    span.appendChild(document.createTextNode(subTask.name));
-    input.setAttribute("type", "checkbox");
-    list.appendChild(input);
-    list.appendChild(span);
-    list.appendChild(icon);
-    unOrderedList.appendChild(list);
-}
-function addNewTaskCategory(newTaskCategory) {
-    var newTask = {
-        id : features.length + 1,
-        icon : "fas fa-list-ul",
-        name: newTaskCategory,
-        tasks:[]
+
+    /**
+     * Function to get one subtask element
+     */
+    function getSubTask(subTask) {
+        const list = document.createElement("li");
+        const input = document.createElement("input");
+        const span = document.createElement("span");
+        span.appendChild(document.createTextNode(subTask.name));
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("name", "check");
+        list.append(input, span);
+        return list;
     }
-    features.push(newTask);
-    var unOrderedList = document.getElementById("left-navigation");
-    var list = document.createElement("li");
-    var icon = document.createElement("i");
-    var span = document.createElement("span");
-    icon.className = newTask.icon;
-    list.appendChild(icon);
-    span.id = newTask.id;
-    span.appendChild(document.createTextNode(newTask.name));
-    list.append(span);
-    unOrderedList.appendChild(list);
-}
-document.addEventListener("click", function(event) {
-    if( "SPAN" == event.target.tagName &&  event.target.parentNode.parentNode.id == "left-navigation") {   
-          document.getElementById("center-title").firstChild.nextSibling.innerHTML = 
-          event.target.textContent;
-          document.getElementById("center-title").firstChild.nextSibling.id = 
-          event.target.id;
-          var subTasks = features[event.target.id - 1].tasks;
-          var taskId = event.target.id;
-          var unOrderedList = document.getElementById("existing-task-list"); 
-          unOrderedList.innerHTML = "";
-          for(var subTask in subTasks) {
-              var list = getOneSubTask(subTasks[subTask], taskId);
-              unOrderedList.appendChild(list);
-          }
-      } 
-  });
-  document.addEventListener("click", function(event) {
-    if( "SPAN" == event.target.tagName && "existing-task-list" === event.target.parentNode.parentNode.id) {
-           document.getElementById("set-sub-task-name").value = event.target.textContent;
-           document.getElementById("set-sub-task-icon").className = event.target.nextSibling.className;
-           document.getElementById("center").id = "center-shrunk";
-           document.getElementById("right").style.visibility = "visible";
-      } 
-  });
-   function getOneSubTask(subTask, taskId) {
-    var list = document.createElement("li");
-    var input = document.createElement("input");
-    var icon = document.createElement("i");
-    var span = document.createElement("span");
-    input.id = subTask.id;
-    if(2 == taskId) {
-        icon.className = "fas fa-star";
-        icon.id = "changeIconBlue";
-    } else {
-        icon.className = "far fa-star";
+
+    /**
+     * Function to render list of tasks of a category in middle part
+     */
+    function renderTask(event) {
+        if ("SPAN" == event.target.tagName && event.target.parentNode.parentNode.id == "left-navigation") {
+            categoryId = event.target.id;
+
+            categoryId = event.target.id;
+            document.getElementsByClassName("center-title")[0].firstChild.nextSibling.innerHTML =
+                event.target.textContent;
+            document.getElementsByClassName("center-title")[0].firstChild.nextSibling.id =
+                event.target.id;
+            const tasks = categories[event.target.id - 1].tasks;
+            taskId = event.target.id;
+            const unOrderedList = document.getElementById("existing-task-list");
+            unOrderedList.innerHTML = "";
+            for (const task in tasks) {
+                const list = getTask(tasks[task]);
+                unOrderedList.appendChild(list);
+            }
+        }
     }
-    
-    span.appendChild(document.createTextNode(subTask.name));
-    input.setAttribute("type", "checkbox");
-    if(true === subTask.checked) {
-        input.setAttribute("checked", "checked");
+
+    /**
+     * Function to render list of sub tasks of a category in right side
+     */
+    function renderSubTask(event) {
+        if ("SPAN" === event.target.tagName && "existing-task-list" === event.target.parentNode.parentNode.id) {
+            taskId = event.target.id;
+            document.getElementsByClassName("set-sub-task-name")[0].textContent = event.target.textContent;
+            document.getElementById("set-sub-task-icon").className = event.target.nextSibling.className;
+            document.getElementById("set-sub-task-icon").style.color = event.target.nextSibling.style.color;
+            document.getElementsByClassName("checkbox-input-right")[0].checked = event.target.previousSibling.checked;
+            document.getElementsByClassName("center-container")[0].style.width = "50%";
+           document.getElementsByClassName("center-container")[0].className = "center-container center-container-shrunk";
+           document.getElementsByClassName("right-container-hidden")[0].className = "right-container";
+        } else if("INPUT" === event.target.tagName) {
+            taskId = event.target.nextSibling.id;
+            reflectCheckedOption(event);
+        } else if("I" === event.target.tagName){
+            taskId = event.target.previousSibling.id;
+            reflectImportantOption(event);
+        }
     }
-    list.appendChild(input);
-    list.appendChild(span);
-    list.appendChild(icon);
-    return list;
-   }
-   document.addEventListener("click", function(event) {
-    if("far fa-star" == event.target.className && "existing-task-list" === event.target.parentNode.parentNode.id) {
-        if("changeIconBlue" != event.target.id) {
-            event.target.id = "changeIconBlue";
+
+    /**
+     * Reflects important option from task in center to right side
+     */
+    function reflectImportantOption(event) {
+        if("fas fa-star" === event.target.className) {   
+            event.target.style.color = "grey";
+            event.target.className = "far fa-star";
+            const task = categories[categoryId - 1].tasks[taskId - 1];
+            categories[categoryId - 1].tasks[taskId - 1].important = false;
+            if(categories[1].tasks.includes(task)) {
+                categories[1].tasks.splice(taskId - 1, 1);                               
+            }  
+            if(event.target.nextSibling.textContent === document.getElementsByClassName("set-sub-task-name")[0].textContent) {
+            document.getElementById("set-sub-task-icon").style.color = "grey";
+            document.getElementById("set-sub-task-icon").className = "far fa-star";
+            }
+        } else { 
+            event.target.style.color = "#0078D7";
             event.target.className = "fas fa-star";
-            var subTaskId = parseInt(event.target.previousSibling.previousSibling.id.charAt(0));
-            var taskCategoryId = document.getElementById("center-title").firstChild.nextSibling.id;
-            var subTask = features[taskCategoryId - 1].tasks[subTaskId - 1];
-            features[1].tasks.push(subTask);
+            const task = categories[categoryId - 1].tasks[taskId - 1];
+            categories[1].tasks.push(task);          
+            categories[categoryId - 1].tasks[taskId - 1].important = true;
+            if(event.target.nextSibling.textContent === document.getElementsByClassName("set-sub-task-name")[0].textContent) {
+            document.getElementById("set-sub-task-icon").style.color = "blue";
+            document.getElementById("set-sub-task-icon").className = "fas fa-star";
+            }
         } 
     }
-});
-init();
+
+    /**
+     * Reflects checked option from task in center to right side
+     */
+    function reflectCheckedOption(event) {
+        if(true === event.target.checked) {
+            categories[categoryId - 1].tasks[taskId - 1].checked = true;
+           if(event.target.nextSibling.textContent === document.getElementsByClassName("set-sub-task-name")[0].textContent) {
+                 document.getElementsByClassName("checkbox-input-right")[0].checked = true;   
+           }
+        } else if (false === event.target.checked) {
+            categories[categoryId - 1].tasks[taskId - 1].checked = false;
+                if(event.target.nextSibling.textContent === document.getElementsByClassName("set-sub-task-name")[0].textContent) {   
+                    document.getElementsByClassName("checkbox-input-right")[0].checked = false;
+                }
+            }   
+    }
+
+    /**
+     * Function to return one row of a task
+     */
+    function getTask(task) {
+        const list = document.createElement("li");
+        const input = document.createElement("input");
+        const icon = document.createElement("i");
+        const span = document.createElement("span");
+        input.id = task.id;
+        if(true == task.important) {
+            icon.className = "fas fa-star";
+            icon.style.color = "#0078D7";
+        } else {
+            icon.className = "far fa-star";
+        }
+        span.appendChild(document.createTextNode(task.name));
+        span.id = task.id;
+        input.setAttribute("type", "checkbox");
+        if(true == task.checked) {
+            input.checked = true;
+        } 
+        list.append(input, span, icon);
+        return list;
+    }
+    init();
 }())
